5b7bdc90017a1b5d6b70be0541d165817af189b1
lloydzhou <lloydzhou@qq.com>
2014-03-27 02:34:29 +0800

    add simple ngx_lua server config to handle upload.

--- client/fileuploader.resume.js
+++ client/fileuploader.resume.js
@@ -8,66 +8,69 @@ qq.extend(qq.FileUploader.prototype, {_createUploadHandler: function (){
 }, _resume_support: function (){
     return 'function' == typeof File && (File.prototype.mozSlice || File.prototype.webkitSlice || File.prototype.slice)
 }})
-qq.extend(qq.UploadHandlerXhr.prototype, {
-    _upload_old: qq.UploadHandlerXhr.prototype._upload, 
-    _upload: function (id, params, part){
-        this._options.resume && this._upload_new(id, params, part) || this._upload_old(id, params);
-    },
-    _upload_new: function(id, params, part){
-        this._options.onUpload(id, this.getName(id), true);
-        part =  part || 1
-        var file = this._files[id],
-            name = this.getName(id),
-            size = this.getSize(id), 
-            BYTES_PER_CHUNK = this._options.resume.chunk || 1024 * 1024, 
-            start = (part - 1) * BYTES_PER_CHUNK, 
-            end = part * BYTES_PER_CHUNK, 
-            slice = file.mozSlice || file.webkitSlice || file.slice, 
-            chunk = slice.call(file,start, end), 
-            xhr = this._xhrs[id] = new XMLHttpRequest(),
-            self = this;
-            
-        xhr.upload.onprogress = function(e){
-            if (e.lengthComputable){
-                self._loaded[id] = e.loaded + start;
-                self._options.onProgress(id, name, e.loaded + start, size);
-            }
-        };
-
-        xhr.onreadystatechange = function(){            
-            if (xhr.readyState == 4){
-                self.log('complete to upload current chunk, start: ' + start + ', part: ' + part)
-                if (end < size && xhr.status == 200)
-                    qq.UploadHandlerXhr.prototype._upload.call(self, id, params, part+1)
-                else
-                    self._onComplete(id, xhr);
-            }
-        };
-
-        // build query string
-        params = params || {};
-        params[this._options.inputName] = name;
-        params['part'] = part;
-        if (end > size)
-            params['merge'] = name;
-        var queryString = qq.obj2url(params, this._options.action);
-
-        xhr.open("POST", queryString, true);
-        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        xhr.setRequestHeader("X-File-Name", encodeURIComponent(name));
-        if (this._options.encoding == 'multipart') {
-            var formData = new FormData();
-            formData.append(name, chunk);
-            chunk = formData;
-        } else {
-            xhr.setRequestHeader("Content-Type", "application/octet-stream");
-            //NOTE: return mime type in xhr works on chrome 16.0.9 firefox 11.0a2
-            xhr.setRequestHeader("X-Mime-Type",file.type );
+qq.backupUpload = function(className, upload){
+    qq.extend(qq[className].prototype, {
+        _upload_old: qq[className].prototype._upload, 
+        _upload: function (id, params, part){
+            this._options.resume && this._upload_new(id, params, part) || this._upload_old(id, params);
+        },
+        _upload_new: upload
+    })
+}
+qq.backupUpload('UploadHandlerXhr', function(id, params, part){
+    this._options.onUpload(id, this.getName(id), true);
+    part =  part || 1
+    var file = this._files[id],
+        name = this.getName(id),
+        size = this.getSize(id), 
+        BYTES_PER_CHUNK = this._options.resume.chunk || 1024 * 1024, 
+        start = (part - 1) * BYTES_PER_CHUNK, 
+        end = part * BYTES_PER_CHUNK, 
+        slice = file.mozSlice || file.webkitSlice || file.slice, 
+        chunk = slice.call(file,start, end), 
+        xhr = this._xhrs[id] = new XMLHttpRequest(),
+        self = this;
+        
+    xhr.upload.onprogress = function(e){
+        if (e.lengthComputable){
+            self._loaded[id] = e.loaded + start;
+            self._options.onProgress(id, name, e.loaded + start, size);
         }
-        for (key in this._options.customHeaders){
-            xhr.setRequestHeader(key, this._options.customHeaders[key]);
-        };
-        xhr.send(chunk);
-        return true
-    },
+    };
+
+    xhr.onreadystatechange = function(){            
+        if (xhr.readyState == 4){
+            self.log('complete to upload current chunk, start: ' + start + ', part: ' + part)
+            if (end < size && xhr.status == 200)
+                qq.UploadHandlerXhr.prototype._upload.call(self, id, params, part+1)
+            else
+                self._onComplete(id, xhr);
+        }
+    };
+
+    // build query string
+    params = params || {};
+    params[this._options.inputName] = name;
+    params['part'] = part;
+    if (end > size)
+        params['merge'] = name;
+    var queryString = qq.obj2url(params, this._options.action);
+
+    xhr.open("POST", queryString, true);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr.setRequestHeader("X-File-Name", encodeURIComponent(name));
+    if (this._options.encoding == 'multipart') {
+        var formData = new FormData();
+        formData.append(name, chunk);
+        chunk = formData;
+    } else {
+        xhr.setRequestHeader("Content-Type", "application/octet-stream");
+        //NOTE: return mime type in xhr works on chrome 16.0.9 firefox 11.0a2
+        xhr.setRequestHeader("X-Mime-Type",file.type );
+    }
+    for (key in this._options.customHeaders){
+        xhr.setRequestHeader(key, this._options.customHeaders[key]);
+    };
+    xhr.send(chunk);
+    return true
 });
